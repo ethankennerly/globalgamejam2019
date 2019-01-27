2b664cfb1aed4c0a67d40a3a938ca5cae0e12d62
Ethan Kennerly <ethan.kennerly@glu.com>
2019-01-27 08:28:26 -0800

    Step Instantiator: Spawns footprint facing up.

--- GlobalGameJam2019/Assets/Scripts/StepInstantiator.cs
+++ GlobalGameJam2019/Assets/Scripts/StepInstantiator.cs
@@ -6,8 +6,31 @@ namespace FineGameDesign.FireFeeder
     {
         [SerializeField]
         private GameObject[] m_PrefabsToInstantiate;
+        private int m_PrefabIndex;
 
         [SerializeField]
-        private float m_DistanceBetweenStep = 0.5f;
+        private float m_DistanceBetweenStep = 4f;
+
+        private Vector3 m_PreviousPosition;
+        private float m_DistanceSinceLastStep;
+
+        private void OnEnable()
+        {
+            m_PreviousPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            m_DistanceSinceLastStep += Vector3.Distance(transform.position, m_PreviousPosition);
+            if (m_DistanceSinceLastStep >= m_DistanceBetweenStep)
+            {
+                m_DistanceSinceLastStep -= m_DistanceBetweenStep;
+                Instantiate(m_PrefabsToInstantiate[m_PrefabIndex], transform.position, Quaternion.identity);
+                ++m_PrefabIndex;
+                if (m_PrefabIndex >= m_PrefabsToInstantiate.Length)
+                    m_PrefabIndex = 0;
+            }
+            m_PreviousPosition = transform.position;
+        }
     }
 }
